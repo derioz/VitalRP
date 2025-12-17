8c4acac5ccd462ac2ab79a464cd2566e889a272f
derioz <txdavidj@gmail.com>
2025-12-16 21:39:48 -0600

    update?

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // For GitHub Pages, we use relative asset paths.
 export default defineConfig({
-  base: "/VitalRP/",
+  base: "/",
   plugins: [react()],
 });
